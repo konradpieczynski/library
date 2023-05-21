1fd8c147ce53716c9913ac4b342acb1d680db920
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-05-21 10:03:37 +0200

    Inititial + documentation

---
+++ src/main/java/com/library/library/LibraryApplication.java
@@ -0,0 +1,20 @@
+package com.library.library;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@OpenAPIDefinition
+@SpringBootApplication
+public class LibraryApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(LibraryApplication.class);
+	}
+}
